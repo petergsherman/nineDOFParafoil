0b3b0844e9928b88ded961073e42b65b53e0b909
petergsherman <petersherman37@gmail.com>
2025-10-19 16:37:01 -0500

    visuals

--- src/nineDOF_Visualization.py
+++ src/nineDOF_Visualization.py
@@ -1,1 +1,8 @@
 #nineDOF_Visualization.py
+import matplotlib.pyplot as plt
+import numpy as np
+
+def plot_3D_trajectory(data, title = "Parafoil 3D Trajectory"):
+    xs = [s[0] for s in data]
+    ys = [s[1] for s in data]
+    zs = [s[2] for s in data]
